1de20aec7d337a7aab9b2b0670d9d8ac2405d506
x6rulin <x6rulin@163.com>
2019-10-31 17:42:31 +0800

    mbstd

--- ganlib/normalize.py
+++ ganlib/normalize.py
@@ -19,9 +19,9 @@ def minibatch_stddev_layer(x, group_size=4, num_new_features=1):
     group_size = min(group_size, x.size(0)) # Minibatch must be divisible by (or smaller than) group_size.
     s = x.shape                             # [NCHW]  Input shape.
     y = torch.reshape(x, (group_size, -1, num_new_features, s[1] // num_new_features, *s[2:])) # [GMncHW]
-    y = torch.mean(y, dim=0, keepdim=True) # [1MncHW] Subtract mean over group.
-    y = torch.mean(y ** 2, dim=0)          # [MncHW]  Calc variance over group.
-    y = torch.sqrt(y + 1e-8)               # [MncHW]  Calc stddev over group.
+    y -= torch.mean(y, dim=0, keepdim=True) # [GMncHW] Subtract mean over group.
+    y = torch.mean(y ** 2, dim=0)           # [MncHW]  Calc variance over group.
+    y = torch.sqrt(y + 1e-8)                # [MncHW]  Calc stddev over group.
     y = torch.mean(y, dim=(2, 3, 4), keepdim=True) # [Mn111] Take average over fmaps and pixels.
     y = torch.mean(y, dim=2)                       # [Mn11]  Split channels into c channel groups.
     y = y.repeat(group_size, num_new_features, *s[2:]) # [NnHW] Replicate over group and pixels.
